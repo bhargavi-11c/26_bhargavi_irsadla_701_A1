ff6aa297216332345dd5f48f506205362c8d8b1d
bhargavi-11c <bhargaviirsadlawork@gmail.com>
2025-07-21 19:09:53 +0530

    Create index.js

---
+++ q11/index.js
@@ -0,0 +1,6 @@
+const utils=require('bhargavi-string')
+
+
+console.log(utils.capitalize("hello"));   // Hello
+console.log(utils.reverse("world"));      // dlrow
+// console.log(utils.isPalindrome("madam")); // true
